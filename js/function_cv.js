fcc466c92267b4aa1a5a2937fc1bdd4d5f6cfd4b
Roberto Gustavo Argañaraz <gustavoargentina2000@mail.com>
2023-02-02 00:32:27 -0300

    primer commit del trabajo 1 CV

---
+++ js/function_cv.js
@@ -0,0 +1,24 @@
+document.getElementById("boton").addEventListener("click", function(){
+    document.getElementById("demo").innerHTML="La Academia Estadounidense de las Artes y las Ciencias (American Academy of Arts and Sciences, AAAS) es una organización dedicada a la enseñanza y el avance del conocimiento. Juega el papel de una asociación honorífica en los Estados Unidos.";
+});
+
+document.getElementById("boton_modo_oscuro").addEventListener("click", function(){
+    document.body.style.backgroundColor = "black";
+    document.body.style.color = "white";
+});
+
+document.getElementById("apple_boton").addEventListener("click", function(){
+    document.getElementById("apple").innerHTML="Macintosh,1​ abreviado como Mac, es la línea de ordenadores personales diseñada, desarrollada y comercializada por Apple Inc. En sus inicios fue una alternativa económica y doméstica al Lisa, un avanzado microordenador empresarial, cuya línea de desarrollo fue absorbida por la línea Macintosh. El Mac terminó por convertirse en la línea estándar de desarrollo de los ordenadores de Apple";
+});
+
+document.getElementById("pixar_boton").addEventListener("click", function(){
+    document.getElementById("pixar").innerHTML="Pixar Animation Studios, más conocido como Pixar, es un estudio cinematográfico de animación por computadora subsidario de Walt Disney Studios y propiedad de The Walt Disney Company con sede en Emeryville, Estados Unidos.2​ Su origen se remonta a 1979, cuando comenzó a operar como The Graphics Group, la división computacional de Lucasfilm.3​ Casi una década después fue adquirida por Steve Jobs, que además invirtió en su capital para establecerla como una empresa independiente de animación.4​ En 1995 Pixar produjo Toy Story, el primer largometraje animado por computadora en la historia del cine.";
+});
+
+document.getElementById("next_boton").addEventListener("click", function(){
+    document.getElementById("next").innerHTML="NeXT Computer, Inc. (nombre que se cambió por el de NeXT Software, Inc.) fue una empresa estadounidense de informática con sede en Redwood City, California, que desarrolló y fabricó una serie de estaciones de trabajo destinadas a la educación superior y las empresas. NeXT fue fundada en 1985 por el fundador de Apple, Steve Jobs, después de su renuncia forzada de Apple. NeXT introdujo el primer NeXT Computer en 1988 y el NeXTstation en 1990.";
+});
+
+document.getElementById("apple_computer_inc_boton").addEventListener("click", function(){
+    document.getElementById("apple_computer_inc").innerHTML="Apple, Inc. es una empresa estadounidense que diseña y produce equipos electrónicos, software y servicios en línea. Tiene su sede principal en el Apple Park, en Cupertino (California, Estados Unidos) y la sede europea en la ciudad de Cork (Irlanda).3​Sus productos de hardware incluyen el teléfono inteligente iPhone, la tableta iPad, la computadora personal Mac, el reproductor de medios portátil iPod, el reloj inteligente Apple Watch y el reproductor de medios digitales Apple TV. Entre el software de Apple se encuentran los sistemas operativos iOS, iPadOS, macOS, watchOS y tvOS, el explorador de contenido multimedia iTunes, la suite iWork (software de productividad), Final Cut Pro X (una suite de edición de vídeo profesional), Logic Pro (software para edición de audio en pistas de audio), Xsan (software para el intercambio de datos entre servidores) y el navegador web Safari.";
+});
